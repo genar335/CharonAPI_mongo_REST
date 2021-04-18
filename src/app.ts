6e4cc88774b28ec78a359dea5a6aca2a88dc8306
Vadims Seinins <seinins.v@gmail.com>
2021-04-18 17:57:25 +0300

    Another Cors update

--- src/app.ts
+++ src/app.ts
@@ -41,12 +41,18 @@ const main = async () => {
 
   app.use(express.json({ limit: `50mb` }));
   app.use(express.urlencoded({ limit: `50mb` }));
-  app.use(
-    cors(
-      // origin: `*`,
-      // credentials: true,
-    )
-  );
+  // app.use(
+  //   cors(
+  //     // origin: `*`,
+  //     // credentials: true,
+  //   )
+  // );
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
   // app.use(fileUpload());
   app.use(express.static(path.join(__dirname, `public`)));
