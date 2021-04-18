c6ebf24845ed30b5105e2563987928b7a75d734f
Vadims Seinins <seinins.v@gmail.com>
2021-04-18 16:21:25 +0300

    more testing of path issues

--- src/app.ts
+++ src/app.ts
@@ -95,7 +95,7 @@ const main = async () => {
   app.post(`${host_url}testimg`, upload.single(`image`), TestController.testFile);
 
   // app.get(`${host_url}`, TestController.getAllTests /* (_, res) => res.send('Hello there!') */)
-  app.get(`${host_url}`, ((_, res: any) => res.send('OIOI')))
+  app.get(`${host_url}`, ((req: express.Request, res: express.Response) => res.send(req)))
 
   app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
