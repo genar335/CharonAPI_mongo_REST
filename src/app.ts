cbd30b34b4a6711bd78d8bff1dd9065f1cb47ecc
Vadims Seinins <seinins.v@gmail.com>
2021-05-04 18:57:40 +0300

    Back to using CORS package

--- src/app.ts
+++ src/app.ts
@@ -50,18 +50,18 @@ const main = async () => {
 
   app.use(express.json({ limit: `50mb` }));
   app.use(express.urlencoded({ limit: `50mb` }));
-  // app.use(cors({
-  //   origin: 'https://vigilant-torvalds-39724e.netlify.app',
-  //   credentials: true,
-  //   // origin: '*'
-  // }))
+  app.use(cors({
+    origin: 'https://vigilant-torvalds-39724e.netlify.app',
+    credentials: true,
+    // origin: '*'
+  }))
   app.use(cookieParser());
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://vigilant-torvalds-39724e.netlify.app"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true")
-    next();
-  });
+  // app.use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "https://vigilant-torvalds-39724e.netlify.app"); // update to match the domain you will make the request from
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   res.header("Access-Control-Allow-Credentials", "true")
+  //   next();
+  // });
 
   // app.use(fileUpload());
   // app.use(express.static(path.join(__dirname, `public`)));
